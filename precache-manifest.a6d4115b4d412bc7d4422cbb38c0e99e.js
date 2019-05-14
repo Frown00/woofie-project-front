e5dabee4e35ed286386944848c4349533906e3db
Fabian Kuriata <fabian.kuriata.dev@gmail.com>
2019-05-14 20:02:19 +0200

    Updates

---
+++ precache-manifest.a6d4115b4d412bc7d4422cbb38c0e99e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbffcf77b88bec5dc790a73b6abb7b8e",
+    "url": "/woofie-project-front/static/media/account.dbffcf77.png"
+  },
+  {
+    "revision": "bb5a2d9f38ab0cfecf5a",
+    "url": "/woofie-project-front/static/css/main.425695ac.chunk.css"
+  },
+  {
+    "revision": "614e4c07753cc45a527a",
+    "url": "/woofie-project-front/static/js/runtime~main.cebe374f.js"
+  },
+  {
+    "revision": "12e65248541c895fba602891f1d52fe8",
+    "url": "/woofie-project-front/static/media/default_avatar.12e65248.jpg"
+  },
+  {
+    "revision": "9df42aa451638ca4f699",
+    "url": "/woofie-project-front/static/js/2.c714d533.chunk.js"
+  },
+  {
+    "revision": "d6149367db0fd1abc7fcddee185eb7b2",
+    "url": "/woofie-project-front/static/media/pimpek.d6149367.jpg"
+  },
+  {
+    "revision": "bb5a2d9f38ab0cfecf5a",
+    "url": "/woofie-project-front/static/js/main.c7e44f07.chunk.js"
+  },
+  {
+    "revision": "19dc82578f842cca438f282e158013d3",
+    "url": "/woofie-project-front/static/media/woman1.19dc8257.jpg"
+  },
+  {
+    "revision": "30bb705e830975517a5e2f578660e648",
+    "url": "/woofie-project-front/static/media/man1.30bb705e.jpg"
+  },
+  {
+    "revision": "06dfeedca4534505117f98a80c9b1ef6",
+    "url": "/woofie-project-front/static/media/woofie_logo.06dfeedc.png"
+  },
+  {
+    "revision": "9df42aa451638ca4f699",
+    "url": "/woofie-project-front/static/css/2.1de0b248.chunk.css"
+  },
+  {
+    "revision": "c208104f42a15cbb7a1324225782b808",
+    "url": "/woofie-project-front/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.47779cf938b8e7cb313c9cb57b92da26.js"
+  "/woofie-project-front/precache-manifest.a6d4115b4d412bc7d4422cbb38c0e99e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/woofie-project-front/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
